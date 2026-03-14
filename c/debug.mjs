e175c4d6718ece48c38067284e27d3ca23169096
Kazuhiro-Tokumoto <20250148@st.shudo-h.ed.jp>
2026-03-14 23:04:31 +0900

    せーかいはへいわ-

--- c/debug.mjs
+++
@@ -1,6 +0,0 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const _mod = require("./ecdsa_wasm.js");
-const ec = await (typeof _mod === "function" ? _mod() : Promise.resolve(_mod));
-console.log("type of ec:", typeof ec);
-console.log("keys with _ec:", Object.keys(ec).filter(k => k.startsWith("_ec")));
